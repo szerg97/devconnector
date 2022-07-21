f8de4b927022d35ffec862829171b09a08a51193
szerg <szerg97@gmail.com>
2022-07-21 11:57:23 +0200

    route works

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 
 const App = () => 
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
-
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={ <Landing /> } />
+        </Routes>
+      </Fragment>
+    </Router>
 export default App;
